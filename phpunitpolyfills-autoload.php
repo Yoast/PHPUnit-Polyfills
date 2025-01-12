1439d08e6282c4a41d897c73370e6c07ae3f02cd
jrfnl <jrfnl@users.noreply.github.com>
2025-01-12 22:18:46 +0100

    Update version number to signal start of 4.x branch

--- phpunitpolyfills-autoload.php
+++ phpunitpolyfills-autoload.php
@@ -22,7 +22,7 @@ if ( \class_exists( 'Yoast\PHPUnitPolyfills\Autoload', false ) === false ) {
 		 *
 		 * @var string
 		 */
-		const VERSION = '3.1.1';
+		const VERSION = '4.0.0-dev';
 
 		/**
 		 * Loads a class.
